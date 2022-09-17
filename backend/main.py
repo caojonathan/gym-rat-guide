ae53590ad366d27184f75a1fd0ee34be17c0a338
caojonathan <jonathancao2@outlook.com>
2022-09-17 15:55:29 -0400

    created models and routes

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,93 @@
-from flask import Flask
-from flask_restx import Api,Resource
+from flask import Flask,request
+from flask_restx import Api,Resource,fields
 from config import DevConfig
+from models import Exercise
+from exts import db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 
+db.init_app(app)
+
+migrate=Migrate(app,db)
 
 api = Api(app,doc='/docs')
+
+exercise_model=api.model( 
+    "Exercise",
+    {
+        "id":fields.Integer(),
+        "title":fields.String(),
+        "description":fields.String()
+    }
+)
+
 
 @api.route('/hello')
 class HelloResource(Resource):
     def get(self):
         return {"message":"Hello World"}
 
+@api.route('/exercises')
+class ExercisesResource(Resource):
+    @api.marshal_list_with(exercise_model)
+    def get(self):
+        """ get all exercises """
+        exercises = Exercise.query.all()
+
+        return exercises
+        
+    @api.marshal_with(exercise_model)
+    def post(self):
+        """ create a new exercises """
+
+        data = request.get_json()
+
+        new_exercise = Exercise(
+            title=data.get('title'),
+            description=data.get('description')
+        )
+
+        new_exercise.save()
+
+        return new_exercise,201
+
+@api.route('/exercises/<int:id>')
+class ExerciseResource(Resource):
+    
+    @api.marshal_with(exercise_model)
+    def get(self,id):
+        """" get exercise by id """
+        exercise=Exercise.query.get_or_404(id)
+        return exercise
+    
+    @api.marshal_with(exercise_model)
+    def put(self,id):
+        """" update an exercise by id"""
+        exercise_to_update = Exercise.query.get_or_404(id)
+
+        data=request.get_json()
+        exercise_to_update.update(data.get('title'),data.get('description'))
+
+        return exercise_to_update
+    
+    @api.marshal_with(exercise_model)
+    def delete(self,id):
+        """" delete an exercise by id"""
+        exercise_to_delete=Exercise.query.get_or_404(id)
+        exercise_to_delete.delete()
+        return exercise_to_delete
+        
+    
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return{
+        "db":db,
+        "Exercise":Exercise
+    }
 
 if __name__ == '__main__':
     app.run()
--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,52 @@
 from curses import keyname
 from exts import db
 
+
+'''
 class Exercise:
     id:int primary key
     title:str
     Description:str (text)
-    
+'''
+
+class Exercise(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    title=db.Column(db.String(), nullable=False)
+    description=db.Column(db.Text(),nullable=False)
+
+    def __repr__(self):
+        return f"<Exercise {self.title} >"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit() 
+
+    def update(self,title,description):
+        self.title=title
+        self.description=description
+
+        db.session.commit()
+
+#user model
+
+"""
+Class User:
+    id:integer
+    username:string
+    email:string
+    password:string
+"""
+
+class User(db.Model): 
+    id=db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, nullable = False, unique = True)
+    email = db.Column(db.String(), nullable=False)
+    password=db.Column(db.Text(),nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
